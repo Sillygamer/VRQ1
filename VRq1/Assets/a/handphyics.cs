6a10cd21cae28aed3b70cbabb144ceb7595de3f2
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-10-06 13:33:04 -0700

    HAND PHYSICS

---
+++ VRq1/Assets/a/handphyics.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handphyics : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rb;
+    private Collider[] handcolliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        handcolliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void enableCollider()
+    {
+        foreach (var item in handcolliders)
+        {
+            item.enabled = true;
+        }
+    }
+
+    public void disableCollider()
+    {
+        foreach (var item in handcolliders)
+        {
+            item.enabled = false;
+        }
+    }
+
+    public void Enabledelay(float delay)
+    {
+        Invoke("enableCollider", delay);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+
+        rotationDifference.ToAngleAxis(out float angleindegree, out Vector3 rotationaxis);
+
+        Vector3 RotationDifrenceindegree = angleindegree * rotationaxis;
+        rb.angularVelocity = (RotationDifrenceindegree * Mathf.Deg2Rad/ Time.fixedDeltaTime);
+    }
+}
