65b8f4adb929cea363b1ead5d6a30895953c1018
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-10-07 11:58:12 -0700

    prfabs now called variant change head and hands and not as parents

--- VRq1/Assets/a/handphyics.cs
+++ VRq1/Assets/a/handphyics.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class handphyics : MonoBehaviour
 {
+
     public Transform target;
     private Rigidbody rb;
     private Collider[] handcolliders;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         handcolliders = GetComponentsInChildren<Collider>();
+       
     }
 
     public void enableCollider()
@@ -22,8 +25,17 @@ public class handphyics : MonoBehaviour
         }
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag != "wall")
+        {
+            Invoke("disableCollider", 0);
+        }
+    }
     public void disableCollider()
     {
+        
+
         foreach (var item in handcolliders)
         {
             item.enabled = false;
