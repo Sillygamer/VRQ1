9f3524da392d66f25f322c6de9f11186931a2af0
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-10-14 12:32:57 -0700

    climbing test on another scene

--- VRq1/Assets/a/ClimbingProvider.cs
+++ VRq1/Assets/a/ClimbingProvider.cs
@@ -1,18 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class ClimbingProvider : MonoBehaviour
+public class ClimbingProvider : LocomotionProvider
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CharacterController characterController;
+
+    private bool isClimbing = false;
+    private List<VelocityContainer> activeVelocitys = new List<VelocityContainer>();
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        FindCharacterController();
+    }
+    private void FindCharacterController()
+    {
+        if (!characterController)
+        {
+            characterController = system.xrOrigin.GetComponent<CharacterController>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddProvider(VelocityContainer provider)
     {
-        
+        if (!activeVelocitys.Contains(provider))
+        {
+            activeVelocitys.Add(provider);
+        }
+    }
+
+    public void RemoveProvider(VelocityContainer provider)
+    {
+        if (activeVelocitys.Contains(provider))
+        {
+            activeVelocitys.Remove(provider);
+        }
+    }
+
+    private void Update()
+    {
+        TryBeginClimb();
+
+        if (isClimbing)
+        {
+            ApplyVelocity();
+        }
+        TryEndClimb();
+    }
+
+    private void TryBeginClimb()
+    {
+        if(CanClimb() && BeginLocomotion())
+        {
+            isClimbing = true;
+        }
+    }
+
+    private void TryEndClimb()
+    {
+        if(!CanClimb()&& BeginLocomotion())
+        {
+            isClimbing = false;
+        }
+    }
+
+    private bool CanClimb()
+    {
+        return activeVelocitys.Count !=0;
+    }
+
+    private void ApplyVelocity()
+    {
+        Vector3 velocity = CollectControllerVelocity();
+        Transform origin = system.xrOrigin.transform;
+
+        velocity = origin.TransformDirection(velocity);
+        velocity *= Time.deltaTime;
+
+        if (characterController)
+        {
+            characterController.Move(-velocity);
+        }
+        else
+        {
+            origin.position -= velocity;
+        }
+    }
+
+    private Vector3 CollectControllerVelocity()
+    {
+        Vector3 totalVelocity = Vector3.zero;
+        foreach(VelocityContainer container in activeVelocitys)
+        {
+            totalVelocity += container.Velocity;
+        }
+
+        return totalVelocity;
     }
 }
