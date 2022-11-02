099da9ad84c72e964c779945b880f33fb474bce4
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-11-02 13:08:50 -0700

    office done

--- VRq1/Assets/a/scripts/officeeventsystem.cs
+++ VRq1/Assets/a/scripts/officeeventsystem.cs
@@ -8,6 +8,9 @@ public class officeeventsystem : MonoBehaviour
 {
     public GameObject emptyrack;
     public GameObject fullhatrack;
+    public GameObject cowboyhat;
+    public GameObject hardhat;
+    public GameObject Tophat;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +26,32 @@ public class officeeventsystem : MonoBehaviour
     {
         emptyrack.SetActive(true);
         fullhatrack.SetActive(false);
+        cowboyhat.SetActive(false);
+        hardhat.SetActive(false);
+        Tophat.SetActive(true);
     }
     public void Changehathardhat()
     {
         emptyrack.SetActive(true);
         fullhatrack.SetActive(false);
+        cowboyhat.SetActive(false);
+        hardhat.SetActive(true);
+        Tophat.SetActive(false);
     }
     public void Changehatfedora()
     {
         emptyrack.SetActive(true);
+        cowboyhat.SetActive(true);
+        hardhat.SetActive(false);
+        Tophat.SetActive(false);
         fullhatrack.SetActive(false);
     }
     public void PutBackhat()
     {
         emptyrack.SetActive(false);
         fullhatrack.SetActive(true);
+        cowboyhat.SetActive(false);
+        hardhat.SetActive(false);
+        Tophat.SetActive(false);
     }
 }
