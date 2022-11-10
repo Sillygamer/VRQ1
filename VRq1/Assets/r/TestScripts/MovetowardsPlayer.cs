3ba4de7513c0c5c1c0dc3e03fd05d06970bd58b0
MUKILTEO\1710152 <Riley05Park@gmail.com>
2022-11-10 13:19:46 -0800

    Added sound

---
+++ VRq1/Assets/r/TestScripts/MovetowardsPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovetowardsPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    public float Movespeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 VectorToPlayer = new Vector3(player.transform.position.x - transform.position.x, 0, player.transform.position.z - transform.position.z).normalized;
+
+        transform.position += VectorToPlayer * Movespeed;
+    }
+}
